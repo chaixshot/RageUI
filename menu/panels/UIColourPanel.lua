4ffa439eee9a2fd249696eab93762571bfc5e690
H@mer <13221018+chaixshot@users.noreply.github.com>
2022-12-02 06:36:52 +0700

    Add files via upload

--- menu/panels/UIColourPanel.lua
+++ menu/panels/UIColourPanel.lua
@@ -101,7 +101,7 @@ function RageUI.ColourPanel(Title, Colours, MinimumIndex, CurrentIndex, Action, 
 
             RageUI.ItemOffset = RageUI.ItemOffset + Colour.Background.Height + Colour.Background.Y
 
-            if (Hovered or LeftArrowHovered or RightArrowHovered) and RageUI.Settings.Controls.Click.Active then
+            if (Hovered or LeftArrowHovered or RightArrowHovered) and (RageUI.Settings.Controls.Click.Active or CurrentMenu.Controls.Left.Active or CurrentMenu.Controls.Right.Active) then
                 local Audio = RageUI.Settings.Audio
                 RageUI.PlaySound(Audio[Audio.Use].Select.audioName, Audio[Audio.Use].Select.audioRef)
             end
