58ad410bf41fbf4a374e638098658b0aad72dfc4
chaixshot <chai5142xshot@gmail.com>
2024-10-19 18:19:14 +0700

    math.round optimize

--- menu/RageUI.lua
+++ menu/RageUI.lua
@@ -12,8 +12,13 @@
 --- @see RageUI
 ---
 
-function math.round(num, numDecimalPlaces)
-    return tonumber(string.format("%." .. (numDecimalPlaces or 0) .. "f", num))
+function math.round(value, numDecimalPlaces)
+	if numDecimalPlaces then
+		local power = 10^numDecimalPlaces
+		return math.floor((value * power) + 0.5) / (power)
+	else
+		return math.floor(value + 0.5)
+	end
 end
 
 function string.starts(String, Start)
